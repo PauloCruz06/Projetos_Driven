6fc10dedcc1bc4c40170e5df4203a6e1967fd54c
Paulo Cruz <pc5783346@gmail.com>
2022-04-01 00:24:24 -0300

    botão de fazer pedido

--- projeto3-driveneats/script.js
+++ projeto3-driveneats/script.js
@@ -1,9 +1,22 @@
+let cont = 0;
+
+
 function selectMainMeal(meal){
     const selectedMeal = document.querySelector(".menu:nth-child(2) .selected");
     if(selectedMeal !== null && selectedMeal !== meal){
         selectedMeal.classList.remove("selected");
+        cont--;
     }
-    meal.classList.toggle("selected");
+    if(selectedMeal === meal){
+        cont--;
+    }
+    meal.classList.add("selected");
+    cont++;
+
+    if(cont == 3){
+    document.querySelector(".button-confirm").classList.remove("hidden");
+    document.querySelector(".button-box").classList.add("hidden");
+    }
 }
 
 
@@ -11,14 +24,34 @@ function selectDrink(meal){
     const selectedMeal = document.querySelector(".menu:nth-child(4) .selected");
     if(selectedMeal !== null && selectedMeal !== meal){
         selectedMeal.classList.remove("selected");
+        cont--;
     }
-    meal.classList.toggle("selected");
+    if(selectedMeal === meal){
+        cont--;
+    }
+    meal.classList.add("selected");
+    cont++;
+
+    if(cont == 3){
+    document.querySelector(".button-confirm").classList.remove("hidden");
+    document.querySelector(".button-box").classList.add("hidden");
+    }
 }
 
 function selectDessert(meal){
     const selectedMeal = document.querySelector(".menu:nth-child(6) .selected");
     if(selectedMeal !== null && selectedMeal !== meal){
         selectedMeal.classList.remove("selected");
+        cont--;
     }
-    meal.classList.toggle("selected");
+    if(selectedMeal === meal){
+        cont--;
+    }
+    meal.classList.add("selected");
+    cont++;
+
+    if(cont == 3){
+    document.querySelector(".button-confirm").classList.remove("hidden");
+    document.querySelector(".button-box").classList.add("hidden");
+    } 
 }
